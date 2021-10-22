0cf8baf8d42fe0528f0a219f47e558db2d900525
sajitha <sajitha@codegen.net>
2021-10-23 00:06:33 +0530

    l function and dist

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.48.0"
+      source = "hashicorp/aws"
+      version = "~> 3.63.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
+
     archive = {
-      source  = "hashicorp/archive"
+      source = "hashicorp/archive"
       version = "~> 2.2.0"
     }
   }
@@ -21,14 +18,30 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "random_pet" "lambda_bucket_name" {
-  prefix = "learn-terraform-functions"
-  length = 4
+
+resource "aws_s3_bucket" "estore_bucket" {
+  bucket = "sj-estore-bucket"
+
+  acl = "private"
+  force_destroy = true
 }
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = random_pet.lambda_bucket_name.id
+//locals {
+//  lambda_dist =
+//}
 
-  acl           = "private"
-  force_destroy = true
+data "archive_file" "lambda_estore_dist" {
+  type = "zip"
+
+  source_dir  = "${path.module}/estore"
+  output_path = "${path.module}/estore.zip"
+}
+
+resource "aws_s3_bucket_object" "lambda_estore" {
+  bucket = aws_s3_bucket.estore_bucket.id
+
+  key    = "estore.zip"
+  source = data.archive_file.lambda_estore_dist.output_path
+
+  etag = filemd5(data.archive_file.lambda_estore_dist.output_path)
 }
--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
 # Output value definitions
 
 output "lambda_bucket_name" {
-  description = "Name of the S3 bucket used to store function code."
-
-  value = aws_s3_bucket.lambda_bucket.id
+  description = "S3 Bucket used to store lambda function code."
+  value = aws_s3_bucket.estore_bucket.id
 }
